598c13351a8c754fb51bd714e65d2761ff7402a0
Guilherme <representacoesgap@gmail.com>
2026-01-12 21:25:21 -0300

    Testando com CLIENT, e incluindo informações

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -5,5 +5,18 @@ export const getLisEpisodes = async (
   response: ServerResponse
 ) => {
   response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify({ name: "Guilherme" }));
+  response.end(JSON.stringify([ {
+        podcastName: "flow",
+        episode: "Fernando Yunes",
+        videoId:"k3XinCZ0u5g",
+        categoria: ['negocios']
+    
+    },
+    {
+         podcastName: "flow",
+        episode: "Germano Couy",
+        videoId: "kBi_0cp_P4g",
+        categoria: ['tecnologia, negocios']
+    }]
+   ));
 };
