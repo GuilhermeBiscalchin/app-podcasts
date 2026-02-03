7c29d4a74b7324499d5c9f217e66686b77938596
Guilherme <representacoesgap@gmail.com>
2026-02-02 23:01:09 -0300

    Criação da pasta models e repositories

--- src/services/list-episodes-service.ts
+++ src/services/list-episodes-service.ts
@@ -1,18 +1,7 @@
-export const serviceListEpisodes = async () => {
-  const data = [
-    {
-      podcastName: "flow",
-      episode: "Fernando Yunes",
-      videoId: "k3XinCZ0u5g",
-      categoria: ["negocios"],
-    },
-    {
-      podcastName: "flow",
-      episode: "Germano Couy",
-      videoId: "kBi_0cp_P4g",
-      categoria: ["tecnologia, negocios"],
-    },
-  ];
+import { repositoryPodcast } from "../repositories/podcasts-repository";
 
-  return data
+export const serviceListEpisodes = async () => {
+  const data = await repositoryPodcast();
+
+  return data;
 };
