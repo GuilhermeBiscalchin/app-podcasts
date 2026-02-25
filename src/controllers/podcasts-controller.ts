0d968b3bc3395a576d1bb6accfc7dfd64d685135
Guilherme <representacoesgap@gmail.com>
2026-02-24 22:23:41 -0300

    Criação da interface, para verificar se ha conteudo,para listar

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -4,6 +4,7 @@ import { serviceListEpisodes } from "../services/list-episodes-service";
 import { serviceFilterEpisodes } from "../services/filter-episodes-service";
 import { StatusCode } from "../utils/status-code";
 import { ContentType } from "../utils/content-type";
+import { PodcastTransferModel } from "../models/filter-podcast-model";
 
 export const getLisEpisodes = async (
   request: IncomingMessage,
@@ -11,16 +12,18 @@ export const getLisEpisodes = async (
 ) => {
   const content = await serviceListEpisodes();
   response.writeHead(StatusCode.OK, { "Content-Type": ContentType.JSON });
-  response.end(JSON.stringify(content));
+  response.write(JSON.stringify(content.body));
+  response.end();
 };
 
 export const getFiltEpisodes = async (
   request: IncomingMessage,
   response: ServerResponse,
 ) => {
-
-
-  const content = await serviceFilterEpisodes(request.url);
-  response.writeHead(StatusCode.OK, { "Content-Type": ContentType.JSON });
-  response.end(JSON.stringify(content));
+  const content: PodcastTransferModel = await serviceFilterEpisodes(
+    request.url,
+  );
+  response.writeHead(content.statusCode, { "Content-Type": ContentType.JSON });
+  response.write(JSON.stringify(content.body));
+  response.end();
 };
--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,13 +1,27 @@
-import { repositoryPodcast } from "../repositories/podcasts-repository"
+import { PodcastTransferModel } from "../models/filter-podcast-model";
+import { repositoryPodcast } from "../repositories/podcasts-repository";
+import { StatusCode } from "../utils/status-code";
 
-
-export const serviceFilterEpisodes = async (podcastName:string | undefined) => {
-
+export const serviceFilterEpisodes = async (
+  podcastName: string | undefined,
+):Promise<PodcastTransferModel> => {
   //localhost:1111/api/episodes?p=flow
   const queryString = podcastName?.split("?p=")[1] || "";
 
-const data = await repositoryPodcast(queryString);
+  let responseFormat: PodcastTransferModel = {
+    statusCode: 0,
+    body: [],
+  };
+  const data = await repositoryPodcast(queryString);
 
-return data
+  //verificar se tem conteúdo na requisição.
+  if (data.length !== 0) {
+    responseFormat.statusCode = StatusCode.OK;
+  } else {
+    responseFormat.statusCode = StatusCode.NO_CONTENT;
+  }
 
-}
+  responseFormat.body = data;
+
+  return responseFormat;
+};
